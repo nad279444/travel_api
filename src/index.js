02f12d16913281173bde8a35a1fe69a04772f554
unknown <nad279444>
2022-02-19 06:38:53 -0800

    added error handling to express-api

--- src/index.js
+++ src/index.js
@@ -20,6 +20,21 @@ app.get('/',(req,res) => {
     })
 })
 
+//error handling
+app.use((req,res,next) => {
+    const error = new Error('this is the wrong route');
+    res.status(404);
+    next(error);
+})
+
+app.use((error,req,res,next) => {
+    const statusCode =  res.statusCode === 200 ? 500: res.statusCode
+    res.json({
+        message: error.message
+    })
+})
+
+
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
